7ba4e0f52f8a6249c5b364eb7a7d083d0366e674
sohamviradiya <sohamviradiya2003@gmail.com>
2023-12-31 12:20:47 +0530

    add update method to Invader class

--- src/modules/invader.ts
+++ src/modules/invader.ts
@@ -9,4 +9,20 @@ export class Invader extends VectorUnit {
         context.fillStyle = 'red';
         context.fillRect(this.x, this.y, this.width, this.height);
     }
+    update(): void {
+        if (this.path.length > 0) {
+            const target = this.path[0];
+            const dx = target.x - this.x;
+            const dy = target.y - this.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+            if (distance < this.speed) {
+                this.path.shift();
+                this.x = target.x;
+                this.y = target.y;
+            } else {
+                this.x += dx / distance * this.speed;
+                this.y += dy / distance * this.speed;
+            }
+        }
+    };
 };
