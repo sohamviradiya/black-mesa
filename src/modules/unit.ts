86f3e9dc4b8fc5df21cc947205a3e0aecdd56014
sohamviradiya <sohamviradiya2003@gmail.com>
2023-12-31 11:56:10 +0530

    Add AlignmentTypes enum and AlignmentType type

--- src/modules/unit.ts
+++ src/modules/unit.ts
@@ -1,24 +1,36 @@
+export enum AlignmentTypes {
+    "NORTH",
+    "SOUTH",
+    "EAST",
+    "WEST"
+};
+
+export type AlignmentType = keyof typeof AlignmentTypes;
+
 export interface StateInterface {
     x: number;
     y: number;
     width: number;
     height: number;
-}
+    alignment: AlignmentType;
+};
 
-export class Unit {
+export class Unit implements StateInterface {
     public width: number;
     public height: number;
-    constructor(public x: number, public y: number, cellSize: number) {
+    constructor(public x: number, public y: number, cellSize: number, public alignment: AlignmentType) {
         this.width = cellSize;
         this.height = cellSize;
     }
     draw(context: CanvasRenderingContext2D, mouse: StateInterface) {
     };
-    update() {
-        
-    }
 
+    update() {
+
+    };
+    
     getState(): StateInterface {
         return this;
     }
 };
+
