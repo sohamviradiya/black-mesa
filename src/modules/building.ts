6ae139479812e9e101e97aa739c476403c5a91d2
sohamviradiya <sohamviradiya2003@gmail.com>
2023-12-31 11:33:38 +0530

    Add building classes for base, generator, and explosive

---
+++ src/modules/building.ts
@@ -0,0 +1,59 @@
+import { StateInterface, Unit } from "./unit";
+
+
+export enum BuildingTypes {
+    "BASE",
+    "TURRET",
+    "EXPLOSIVE",
+    "GENERATOR",
+};
+
+export type BuildingType = keyof typeof BuildingTypes;
+
+export class Building extends Unit {
+    public width: number;
+    public height: number;
+    constructor(public x: number, public y: number, cellSize: number, public type: BuildingType, public cost: number = 0) {
+        super(x, y, cellSize);
+        this.width = cellSize;
+        this.height = cellSize;
+    }
+}
+
+class Installation extends Building {
+    constructor(x: number, y: number, cellSize: number, public health: number, public rate: number, public type: BuildingType) {
+        super(x, y, cellSize, type);
+    };
+};
+
+export class Base extends Installation {
+    constructor(x: number, y: number, cellSize: number, health: number, rate: number) {
+        super(x, y, cellSize, health, rate, "BASE");
+    };
+    draw(context: CanvasRenderingContext2D, mouse: StateInterface): void {
+        context.fillStyle = 'blue';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+
+
+export class Generator extends Installation {
+    constructor(x: number, y: number, cellSize: number, cost: number, rate: number) {
+        super(x, y, cellSize, 100, rate, "GENERATOR");
+    };
+    draw(context: CanvasRenderingContext2D, mouse: StateInterface): void {
+        context.fillStyle = 'green';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
+
+
+export class Explosive extends Building {
+    constructor(x: number, y: number, cellSize: number, public cost: number, public damage: number) {
+        super(x, y, cellSize, "EXPLOSIVE", cost);
+    };
+    draw(context: CanvasRenderingContext2D, mouse: StateInterface): void {
+        context.fillStyle = 'red';
+        context.fillRect(this.x, this.y, this.width, this.height);
+    }
+};
