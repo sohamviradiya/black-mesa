ab3a3e770a6b161622b2e2f35dd4accbb399d97d
sohamviradiya <sohamviradiya2003@gmail.com>
2023-12-31 14:07:58 +0530

    Add id property to Unit class and extend VectorUnit from Unit

--- src/modules/unit.ts
+++ src/modules/unit.ts
@@ -25,9 +25,13 @@ export enum AlignmentTypes {
 export type AlignmentType = keyof typeof AlignmentTypes;
 
 export class Unit implements StateInterface {
+    static nextId = 0;
     public width: number;
     public height: number;
+    public id: number;
     constructor(public x: number, public y: number, cellSize: number) {
+        this.id = Unit.nextId;
+        Unit.nextId++;
         this.width = cellSize;
         this.height = cellSize;
     }
@@ -40,9 +44,11 @@ export class Unit implements StateInterface {
     }
 };
 
-export class VectorUnit implements VectorStateInterface {
+export class VectorUnit extends Unit implements VectorStateInterface {
     constructor(public x: number, public y: number, public width: number, public height: number, public angle: number) {
-        
+        super(x, y, width);
+        this.width = width;
+        this.height = height;
     }
     draw(context: CanvasRenderingContext2D, mouse: StateInterface) { };
     update() { };
