63f00b8a9367b072368d6db5dc9253f628b76ff9
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-01 10:30:46 +0530

    Add routing functionality to Root component

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import Main from './pages/Main';
+import { Outlet, RouterProvider } from '@tanstack/react-router';
+import { Router, Route, RootRoute } from '@tanstack/react-router';
+
+const root = new RootRoute({
+  component: () => (<Outlet />),
+});
+
+
+const MainRoute = new Route({
+  path: '/',
+  component: Main,
+  getParentRoute: () => root,
+});
+
+const routeTree = root.addChildren([MainRoute]);
+
+const router = new Router({ routeTree });
+
 
 function Root() {
   return (
-    <Main />
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
