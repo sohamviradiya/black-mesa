6e31e2c1e79d4d0632f0f9ba3e624269b623d1cf
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-01 17:23:19 +0530

    Add invader spawning and update methods to BoardState class

--- src/modules/state.ts
+++ src/modules/state.ts
@@ -10,7 +10,22 @@ export class BoardState {
     static nextFrame: number = 0;
     public frame: number = BoardState.nextFrame++;
     constructor(public collections: { projectiles: Projectile[], invaders: Invader[], base: Base, buildings: Building[], cells: Cell[] }, public energy: number, public score: number, public gameOver: boolean, public gameWon: boolean, public mouse: ScalarInterface) {
-
+        // TODO: set up invader spawning
+    };
+    update(state: BoardState): void {
+        state.collections.projectiles.forEach((projectile: Projectile) => projectile.update(state));
+        state.collections.invaders.forEach((invader: Invader) => invader.update(state));
+        state.collections.buildings.forEach((building: Building) => building.update(state));
+        state.collections.cells.forEach((cell: Cell) => cell.update(state));
+    }
+    components(): JSX.Element[] {
+        return [
+            this.collections.base.component(),
+            ...this.collections.buildings.map((building: Building) => building.component()),
+            ...this.collections.cells.map((cell: Cell) => cell.component()),
+            ...this.collections.invaders.map((invader: Invader) => invader.component()),
+            ...this.collections.projectiles.map((projectile: Projectile) => projectile.component()),
+        ];
     }
 }
 
