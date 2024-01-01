bc99263a098008bf0c04ef98f20f2403d63f5f53
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-01 18:38:54 +0530

    move explosive to a new file

---
+++ src/modules/explosive.ts
@@ -0,0 +1,46 @@
+import { Building } from "./building";
+import { Invader } from "./invader";
+import { collision, isInRadius } from "./geometry";
+import { BoardState } from "./state";
+import ExplosiveComponent from "../components/units/explosive";
+
+
+export interface ExplosiveTemplate {
+    cost: number;
+    damage: number;
+    radius: number;
+};
+
+export class Explosive extends Building {
+    public triggered: boolean = false;
+    constructor(row_index: number, column_index: number, cellSize: number, public template: ExplosiveTemplate) {
+        super(row_index, column_index, cellSize, "EXPLOSIVE", template.cost);
+    };
+
+    update(state: BoardState): void {
+        if (this.triggered) {
+            this.explode(state.collections.invaders);
+            this.removeSelf(state);
+        }
+
+        state.collections.invaders.forEach((invader: Invader) => {
+            if (collision(this, invader)) {
+                this.triggered = true;
+                return;
+            }
+        });
+    }
+
+    component(): JSX.Element {
+        return ExplosiveComponent({ explosive: this });
+    }
+
+    private explode(invaders: Invader[]) {
+        invaders.forEach((invader: Invader) => {
+            if (isInRadius(this, invader)) {
+                invader.takeDamage(this.template.damage);
+            }
+        });
+    }
+}
+;
