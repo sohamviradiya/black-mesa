f20371535aed0a8803785c4ac3d521eff10df602
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-01 18:27:39 +0530

    Refactor Projectile class constructor to use a template object

--- src/modules/projectiles.ts
+++ src/modules/projectiles.ts
@@ -4,11 +4,18 @@ import { Invader } from "./invader";
 import { BoardState, CollectionType } from "./state";
 import { VectorUnit } from "./unit";
 
+export interface ProjectileTemplate {
+    speed: number;
+    damage: number;
+    width: number;
+    height: number;
+};
+
 export class Projectile extends VectorUnit {
     public active: boolean = true;
     collection: CollectionType = "projectiles";
-    constructor(row_index: number, column_index: number, width: number, height: number, angle: number, public speed: number, public target: Invader, public damage: number) {
-        super(row_index, column_index, width, height, angle);
+    constructor(row_index: number, column_index: number, angle: number,  public target: Invader,public template: ProjectileTemplate) {
+        super(row_index, column_index, template.width, template.height, angle);
     }
 
     update(state: BoardState): void {
@@ -23,7 +30,7 @@ export class Projectile extends VectorUnit {
         }
 
         if (collision(this, this.target)) {
-            this.target.takeDamage(this.damage);
+            this.target.takeDamage(this.template.damage);
             this.active = false;
             return;
         }
@@ -31,14 +38,14 @@ export class Projectile extends VectorUnit {
         const dx = this.target.x - this.x;
         const dy = this.target.y - this.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
-        if (distance < this.speed) {
+        if (distance < this.template.speed) {
             this.x = this.target.x;
             this.y = this.target.y;
             return;
         }
 
-        this.x += dx / distance * this.speed;
-        this.y += dy / distance * this.speed;
+        this.x += dx / distance * this.template.speed;
+        this.y += dy / distance * this.template.speed;
         this.angle = Math.atan2(dy, dx);
     };
 
