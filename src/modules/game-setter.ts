36efa2bd440273918da8e21b013253134d9d5687
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-01 21:30:06 +0530

    Add game variables and difficulty mapper

---
+++ src/modules/game-setter.ts
@@ -0,0 +1,43 @@
+
+export type Difficulty = 'ROOKIE' | 'CASUAL' | 'MASTER' | 'VETERAN' | 'INSANE'; 
+
+export function difficultyMapper(difficulty: Difficulty) {
+    if (difficulty === "ROOKIE") {
+        return {
+            rows: 6, 
+            columns: 12,
+            turnFactor: 0.9,
+        }
+    }
+    else if (difficulty === "CASUAL") {
+        return {
+            rows: 10,
+            columns: 15,
+            turnFactor: 0.75,
+        }
+    }
+    else if (difficulty === "MASTER") {
+        return {
+            rows: 15,
+            columns: 24,
+            turnFactor: 0.6,
+        }
+    }
+    else if (difficulty === "VETERAN") {
+        return {
+            rows: 20,
+            columns: 40,
+            turnFactor: 0.5,
+        }
+    }
+    else if (difficulty === "INSANE") {
+        return {
+            rows: 30,
+            columns: 50,
+            turnFactor: 0.25,
+        }
+    }
+    else {
+        throw new Error("Invalid difficulty");
+    }
+}
