d5005bbbb97cf8283aac27987a030df72d03ef49
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-02 22:06:21 +0530

    Update import path for Generator component

--- src/components/units/generator.tsx
+++ src/components/units/generator.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-import { Generator } from "../../modules/generator";
+import { Generator } from "../../modules/buildings/generator";
 
 export default function GeneratorComponent({ generator }: { generator: Generator }) {
     return <Box sx={{ minWidth: "40px", minHeight: "40px", backgroundColor: "yellow", border: "1px solid black" }} />;
