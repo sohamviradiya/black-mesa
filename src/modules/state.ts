1f926c716863e9edcb362214719ec2202c8bdd4f
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-03 15:34:11 +0530

    set up invader spawning and defense add

--- src/modules/state.ts
+++ src/modules/state.ts
@@ -1,56 +1,52 @@
-import { Building } from "./building";
+import { Building, BuildingTemplate } from "./building";
 import { Base, BaseTemplate } from "./buildings/base";
 import { Cell, PathCell, SlotCell } from "./cell";
-import { Invader, InvaderTemplate } from "./invader";
+import { Invader, InvaderType } from "./invader";
 import { Projectile } from "./projectiles";
 import { PositionInterface, ScalarInterface } from "./unit";
 import { generateGrid } from "./grid";
-import variables from "../data/game-variables.json";
-import buildings from "../data/buildings.json";
-import difficultyVariables from "../data/difficulty-mappers.json";
 import { InstallationType } from "./buildings/installation";
 import { Generator, GeneratorTemplate } from "./buildings/generator";
 import { Barricade, BarricadeTemplate } from "./buildings/barricade";
 import { Explosive, ExplosiveTemplate } from "./buildings/explosive";
 import { matrixToCells } from "./cell";
 import { pathToPositions } from "./geometry";
+import { Defense, DefenseTemplate, DefenseType, ScalarTurret, VectorTurret, VectorDefenseTemplate } from "./buildings/defenses";
+
+import difficultyVariables from "../data/difficulty-mappers.json";
+import variables from "../data/game-variables.json";
+import buildings from "../data/buildings.json";
+import defenses from "../data/defenses.json";
+import invaders from "../data/invaders.json";
 
 export type CollectionType = "projectiles" | "invaders" | "buildings" | "cells";
 
 export type Difficulty = "ROOKIE" | "CASUAL" | "MASTER" | "VETERAN" | "INSANE";
 
-export type InvaderSpawn = {
-    template: InvaderTemplate,
-    timeout: number,
-};
-
 export class BoardState {
     public frame: number = 0;
-    lastInvaderSpawn: number = 0;
-    public collections: { projectiles: Projectile[], invaders: Invader[], buildings: Building[], cells: Cell[][] };
+    public collections: { projectiles: Projectile[], invaders: Invader[], buildings: Building[], cells: Cell[][] } = { projectiles: [], invaders: [], buildings: [], cells: [] };
     public energy: number = variables["initial-energy"];
     public score: number = 0;
     public gameOver: boolean = false;
     public gameWon: boolean = false;
-    public mouse: ScalarInterface;
+    public mouse: ScalarInterface = { x: 0, y: 0, width: variables["mouse"]["width"], height: variables["mouse"]["height"] };
     public messages: string[] = [];
-    invaderSpawns: InvaderSpawn[];
     public path: PositionInterface[];
+    private invaderQueue: InvaderType[] = [];
+    private invaderPeriod: number = 0;
+
     constructor(cellSize: number, difficulty: Difficulty) {
         const { rows, columns, turnFactor } = difficultyVariables[difficulty];
-        this.invaderSpawns = [];
+        this.setInvaderQueue(difficulty);
+        this.invaderPeriod = difficultyVariables[difficulty]["invader-period"];
 
         const { matrix, path } = generateGrid(rows, columns, turnFactor);
-        this.mouse = { x: 0, y: 0, width: variables["mouse"]["width"], height: variables["mouse"]["height"] };
 
-        this.collections = {
-            projectiles: [],
-            invaders: [],
-            cells: matrixToCells(matrix, cellSize),
-            buildings: [],
-        };
+        this.collections.cells = matrixToCells(matrix, cellSize);
 
         this.path = pathToPositions(path, cellSize);
+
         const { row_index: base_row_index, column_index: base_column_index } = path[path.length - 1];
         this.addBase(this.collections.cells[base_row_index][base_column_index] as PathCell);
     }
@@ -65,7 +61,7 @@ export class BoardState {
             this.gameWon = true;
             return;
         }
-        this.updateInvaders();
+        this.spawnInvaders();
         this.updateCollections();
     }
 
@@ -84,26 +80,24 @@ export class BoardState {
         this.messages.push(message);
     }
 
-    isGameWon(): boolean {
-        return (this.collections.invaders.length === 0 && this.invaderSpawns.length === 0);
-    }
+    addDefense(cell: SlotCell, type: DefenseType): void {
+        const template = defenses[type];
+        if (!this.checkCost(template as BuildingTemplate))
+            return;
+        const { row_index, column_index } = cell;
+        let defense: Defense;
+        if (type === "SNIPER")
+            defense = new VectorTurret(row_index, column_index, template as VectorDefenseTemplate);
+        else
+            defense = new ScalarTurret(row_index, column_index, template as DefenseTemplate);
 
-    updateInvaders() {
-        const { timeout, template } = this.invaderSpawns[0];
-        if (timeout === this.frame - this.lastInvaderSpawn) {
-            this.collections.invaders.push(new Invader(this.path, template));
-            this.invaderSpawns.shift();
-            this.lastInvaderSpawn = this.frame;
-        }
-    }
-
-    addBuilding(slot: SlotCell, type: InstallationType): void {
-
+        if (cell.occupy(defense))
+            this.energy -= template.cost;
     }
 
     addExplosive(cell: PathCell) {
         const template = buildings["EXPLOSIVE"];
-        if (this.energy < template.cost)
+        if (!this.checkCost(template as BuildingTemplate))
             return;
         const { row_index, column_index } = cell;
         const explosive = new Explosive(row_index, column_index, template as ExplosiveTemplate);
@@ -122,10 +116,8 @@ export class BoardState {
 
     private addInstallation(cell: PathCell, type: InstallationType): void {
         const template = buildings[type];
-        if (this.energy < template.cost) {
-            this.addMessage("Not enough energy to build " + type);
+        if (!this.checkCost(template as BuildingTemplate))
             return;
-        }
         const { row_index, column_index } = cell;
         let installation;
         if (template.type === "GENERATOR")
@@ -140,6 +132,45 @@ export class BoardState {
         this.collections.buildings.push(installation);
         if (cell.occupy(installation))
             this.energy -= template.cost;
+    }
+
+    private checkCost(template: BuildingTemplate) {
+        if (this.energy < template.cost) {
+            this.addMessage("Not enough energy to build " + template.type);
+            return false;
+        }
+        return true;
+    }
+
+    private isGameWon(): boolean {
+        return (this.collections.invaders.length === 0 && this.invaderQueue.length === 0);
+    }
+
+    private spawnInvaders() {
+        if (this.frame % this.invaderPeriod !== 0 || this.invaderQueue.length === 0)
+            return;
+        const invaderType: InvaderType = this.invaderQueue.shift() as InvaderType;
+        const invader = new Invader(this.path, invaders[invaderType]);
+        this.collections.invaders.push(invader);
+    }
+
+    private setInvaderQueue(difficulty: Difficulty) {
+        const count = difficultyVariables[difficulty].invaderSpawns;
+        const invaderQueue: InvaderType[] = [];
+
+        Object.keys(count).forEach((key) => {
+            const invaderType: InvaderType = key as InvaderType;
+            const invaderCount: number = count[key];
+            for (let i = 0; i < invaderCount; i++)
+                invaderQueue.push(invaderType);
+        });
+
+        for (let i = invaderQueue.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [invaderQueue[i], invaderQueue[j]] = [invaderQueue[j], invaderQueue[i]];
+        }
+
+        this.invaderQueue = invaderQueue;
     }
 
     private updateCollections() {
