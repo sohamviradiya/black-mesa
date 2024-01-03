d725205f448338edf075664cca5986778847d5b6
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-03 22:48:32 +0530

    Update defense and invader components

--- src/components/units/buildings/defense.tsx
+++ src/components/units/buildings/defense.tsx
@@ -4,7 +4,7 @@ import { Defense } from "../../../modules/buildings/defenses";
 export default function DefenseComponent({ defense, children }: { defense: Defense, children: React.ReactNode }) {
     return <>
         {children}
-        <Typography variant="h6" sx={{ textAlign: "center" }}> {defense.template.type} </Typography>
+        <Typography variant="h6" sx={{ textAlign: "center" }}> {defense.template.weapon} </Typography>
     </>;
 };
 
--- src/components/units/invader.tsx
+++ src/components/units/invader.tsx
@@ -2,9 +2,9 @@ import { Box, Typography } from "@mui/material";
 import { Invader } from "../../modules/invader";
 
 export default function InvaderComponent({ invader }: { invader: Invader }) {
-    return <Box sx={{ width: "80%", height: "80%", backgroundColor: "grey", border: "1px solid black", }} >
-        <Typography variant="h6" sx={{ textAlign: "center" }}> {invader.health} </Typography>
+    return <Box sx={{ width: invader.height, height: invader.width, backgroundColor: "grey", border: "1px solid black", }} >
         <Typography variant="body1" sx={{ textAlign: "center" }}> {invader.template.type} </Typography>
+        <Typography variant="body2" sx={{ textAlign: "center" }}> {invader.health} </Typography>
     </Box>;
 };
 
