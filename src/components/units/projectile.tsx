951504c733c8b5d67dc790ad2207cf1f4956c7b8
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-04 16:05:40 +0530

    Update ProjectileComponent styling

--- src/components/units/projectile.tsx
+++ src/components/units/projectile.tsx
@@ -2,6 +2,6 @@ import { Box } from "@mui/material";
 import { Projectile } from "../../modules/projectile";
 
 export default function ProjectileComponent({ projectile }: { projectile: Projectile }) {
-    return <Box sx={{ backgroundColor: "dimgrey", border: "1px solid black" }} />;
+    return <Box sx={{ backgroundColor: "dimgrey", border: "1px solid black", height: "100%", width: "100%" }} />;
 };
 
