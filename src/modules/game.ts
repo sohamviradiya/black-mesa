1263257b75f0bba7ac58134356294ab5b61df286
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-04 18:06:14 +0530

    Remove gameLoop function from game.ts

--- src/modules/game.ts
+++
@@ -1,6 +0,0 @@
-import { BoardState } from "./state";
-
-export function gameLoop(setBoardState: React.Dispatch<React.SetStateAction<BoardState>>) {
-    setBoardState((state: BoardState) => state.update());
-    requestAnimationFrame(() => gameLoop(setBoardState));
-}
