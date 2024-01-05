e1215d4d116a7fe66ffc7253ddac2d24ce65edc5
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-05 14:03:36 +0530

    increase explosive timer

--- src/components/units/buildings/defenses/explosive.tsx
+++ src/components/units/buildings/defenses/explosive.tsx
@@ -1,8 +1,8 @@
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Explosive } from "../../../../modules/buildings/explosive";
 
 export default function ExplosiveComponent({ explosive }: { explosive: Explosive }) {
-    return <Box sx={{ border: "1px solid black" }}>
+    return <>
         <Typography variant="h6">{explosive.timer}</Typography>
-    </Box>;
+    </>;
 };
