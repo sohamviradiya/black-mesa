d830954562f4a825a28886d8da14fdaaa2c63ad3
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-05 13:12:49 +0530

    Add symbol for building type in BuildingComponent

--- src/components/units/building.tsx
+++ src/components/units/building.tsx
@@ -1,9 +1,21 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { Building } from "../../modules/building";
 
 export default function BuildingComponent({ building, children }: { building: Building, children: React.ReactNode }) {
-    return <>
+    if (building.type === "DEFENSE") return <>{children}</>;
+
+    return <Box sx={{ border: "2px solid blue", width: "100%", height: "100%" }} >
+        <Typography variant="h3" sx={{ textAlign: "center" }}> {getSymbol(building)} </Typography>
         {children}
-        <Typography sx={{ textAlign: "center" }} >{building.type}</Typography>
-    </>
+    </Box>;
 };
+
+function getSymbol(building: Building) {
+    switch (building.type) {
+        case "BASE": return "🏛";
+        case "DEFENSE": return "";
+        case "GENERATOR": return "🔋";
+        case "BARRICADE": return "🚧";
+        case "EXPLOSIVE": return "💣";
+    }
+}
