e6ca4b95ef6d43ca19a01067bf5dbacd51b0839a
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-05 14:15:56 +0530

    fix building demolishing

--- src/components/units/building.tsx
+++ src/components/units/building.tsx
@@ -2,17 +2,14 @@ import { Box, Typography } from "@mui/material";
 import { Building } from "../../modules/building";
 
 export default function BuildingComponent({ building, children, demolishBuilding }: { building: Building, children: React.ReactNode, demolishBuilding: (building: Building) => void }) {
-    if (building.type === "DEFENSE") return <>{children}</>;
 
     return <Box sx={{ border: "2px solid blue", width: "100%", height: "100%" }} onContextMenu={
         (e) => {
             e.preventDefault();
-            e.stopPropagation();
-            console.log("demolish building");
             demolishBuilding(building);
         }
     } >
-        <Typography variant="h3" sx={{ textAlign: "center" }}> {getSymbol(building)} </Typography>
+        {building.type !== "DEFENSE" && <Typography variant="h3" sx={{ textAlign: "center" }}> {getSymbol(building)} </Typography>}
         {children}
     </Box>;
 };
--- src/components/units/buildings/defenses/vector-turret.tsx
+++ src/components/units/buildings/defenses/vector-turret.tsx
@@ -3,7 +3,6 @@ import { VectorTurret } from "../../../../modules/buildings/defenses";
 
 export default function VectorTurretComponent({ turret }: { turret: VectorTurret }) {
     return <Box sx={{ border: "1px solid black" }} onClick={() => turret.setAlignment()}>
-        <Typography variant="h6" sx={{ textAlign: "center" }}> {turret.angle} </Typography>
         <Typography variant="h6" sx={{ textAlign: "center" }}> {turret.alignment} </Typography>
     </Box>;
 };
