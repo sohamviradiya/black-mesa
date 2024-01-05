da55dfee86afadbf7f3d434f3c4d77071ef64a31
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-05 13:12:54 +0530

    Refactor DefenseComponent to display weapon symbols

--- src/components/units/buildings/defense.tsx
+++ src/components/units/buildings/defense.tsx
@@ -1,10 +1,21 @@
-import {  Typography } from "@mui/material";
-import { Defense } from "../../../modules/buildings/defenses";
+import { Box, Typography } from "@mui/material";
+import { Defense, WeaponType } from "../../../modules/buildings/defenses";
 
 export default function DefenseComponent({ defense, children }: { defense: Defense, children: React.ReactNode }) {
-    return <>
-        {children}
-        <Typography variant="h6" sx={{ textAlign: "center" }}> {defense.template.weapon} </Typography>
-    </>;
+    return <Box sx={{ border: "2px solid red", width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }} >
+        <Box sx={{ transform: `rotate(${defense.angle+Math.PI}rad)` }}>
+            <Typography variant="h3" sx={{ textAlign: "center" }}> {getSymbol(defense.template.weapon)} </Typography>
+            {children}
+        </Box>
+    </Box>;
 };
 
+function getSymbol(type: WeaponType) {
+    switch (type) {
+        case "LASER": return "🔫";
+        case "SNIPER": return "🎯";
+        case "MISSILE_LAUNCHER": return "🚀";
+        case "MACHINE_GUN": return "🎆";
+        case "SHOTGUN": return "🎇";
+    }
+}
