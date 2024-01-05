2358353ef3c083f57f0e0957f3dfab933d0b3ddf
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-05 14:01:49 +0530

    Refactor turret component styles

--- src/components/units/buildings/defenses/vector-turret.tsx
+++ src/components/units/buildings/defenses/vector-turret.tsx
@@ -2,7 +2,7 @@ import { Box, Typography } from "@mui/material";
 import { VectorTurret } from "../../../../modules/buildings/defenses";
 
 export default function VectorTurretComponent({ turret }: { turret: VectorTurret }) {
-    return <Box sx={{ backgroundColor: "darkcyan", border: "1px solid black" }} onClick={() => turret.setAlignment()}>
+    return <Box sx={{ border: "1px solid black" }} onClick={() => turret.setAlignment()}>
         <Typography variant="h6" sx={{ textAlign: "center" }}> {turret.angle} </Typography>
         <Typography variant="h6" sx={{ textAlign: "center" }}> {turret.alignment} </Typography>
     </Box>;
