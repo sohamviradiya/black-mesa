03f2306d417745fa9fe106a9cbdfc04ef6791a22
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-06 20:48:10 +0530

    Add projectile images and display them in the ProjectileComponent

--- src/components/units/projectile.tsx
+++ src/components/units/projectile.tsx
@@ -1,7 +1,28 @@
 import { Box } from "@mui/material";
 import { Projectile } from "../../modules/projectile";
+import sniperBulletImage from "../../assets/images/sniper-bullet.svg";
+import machineGunBulletImage from "../../assets/images/machine-gun-bullet.svg";
+import missileImage from "../../assets/images/missile.svg";
+import shotgunBulletImage from "../../assets/images/shot-gun-bullet.svg";
+import laserBulletImage from "../../assets/images/laser-bullet.svg";
 
 export default function ProjectileComponent({ projectile }: { projectile: Projectile }) {
-    return <Box sx={{ backgroundColor: "dimgrey", border: "1px solid black", height: "100%", width: "100%" }} />;
+    return <Box sx={{ backgroundColor: "dimgrey", height: "100%", width: "100%" }} >
+        <img src={getProjectileImage(projectile)} alt="projectile" style={{ width: projectile.width, height: projectile.height }} />
+    </Box>
 };
 
+function getProjectileImage(projectile: Projectile) {
+    switch (projectile.template.name) {
+        case "SNIPER":
+            return sniperBulletImage;
+        case "MACHINE_GUN":
+            return machineGunBulletImage;
+        case "MISSILE_LAUNCHER":
+            return missileImage;
+        case "SHOTGUN":
+            return shotgunBulletImage;
+        case "LASER":
+            return laserBulletImage;
+    }
+}
