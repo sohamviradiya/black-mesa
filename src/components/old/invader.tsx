713e2268b88486fe430ba59990fee68865528a42
sohamviradiya <sohamviradiya2003@gmail.com>
2024-01-06 18:05:49 +0530

    Remove old canvas, invader, and turret components

--- src/components/old/invader.tsx
+++
@@ -1,8 +0,0 @@
-import { Invader } from "../../modules/invader";
-
-export default function InvaderComponent({ invader }: { invader: Invader }) {
-    return (
-        <div style={{ position: "absolute", top: invader.x, left: invader.y, width: invader.width, height: invader.height, backgroundColor: "purple" }}>
-        </div>
-    );
-};
--- src/components/old/turret.tsx
+++
@@ -1,9 +0,0 @@
-import { ScalarTurret } from "../../modules/buildings/defenses";
-
-export default function TurretComponent({ turret }: { turret: ScalarTurret }) {
-    return (
-        <div style={{ position: "absolute", top: turret.y, left: turret.x, width: turret.width, height: turret.height, backgroundColor: "red", zIndex: 1 }}>
-
-        </div>
-    );
-};
